xen-troops/meta-xt-images

commit 10628d11c9af73074dd6cdd2ae6c7ae7ec072d41

doma: rework Android build
Android build is moved from lower yocto to upper.
Almost all functions and settings are moved to
xt-images' domu-image-android recipe.
Also we use default OUT_DIR now as paths are quite short.

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

recipes-domu/domu-image-android/domu-image-android.bb
@@ -1,76 +1,123 @@
-SUMMARY = "Base for Android images"
+SUMMARY = "Base for Android build"
 
 LICENSE = "MIT"
 
-inherit build_yocto
-inherit xt_quirks
-inherit xt_reconstruct
+require inc/xt_shared_env.inc
 
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
-FILESEXTRAPATHS_prepend := "${THISDIR}/../../classes:"
+# Important note.
+# We use REPODIR and SRC_URI with 'directpath=1' to bypass fetcher's
+# modification of download path. This is done for performance reasons.
+#
+# 'Out-of-stock' repo fetcher gets sources into ${DL_DIR}/repo/xxx folder,
+# then tar/gzip them and then ungzip to ${S}.
+# These steps add almost one hour to fetching on PC.
+#
+# With modifications to xt-distro's repo fetcher (adding 'depth' and 'directpath'
+# parameters to SRC_URI) and using REPODIR instead of DL_DIR (to avoid copying),
+# we have do_fetch() completed within 20 minutes.
+#
+# Some details:
+# 'depth=1' reduces amount of data downloaded from server, and thus reduces fetch time
+#           in half: ~(10-14)min instead of ~23min
+# 'REPODIR' is used to download sources directly into required place,
+#           and to avoid copying of tens of gigabytes from ${DL_DIR}
+# 'directpath=1' is used to suppress creation of subfolders to ${REPODIR}.
+#           This is required for correct work of do_compile() with downloaded sources.
 
-S = "${WORKDIR}/repo"
+REPODIR = "${SSTATE_DIR}/../android_sources"
+S = "${REPODIR}"
 
-################################################################################
-# set to AUTOREV so we can override this while reconstructing this build with
-# specific commit ID
-################################################################################
-SRCREV = "${AUTOREV}"
+ANDROID_SOURCES = "repo://github.com/xen-troops/android_manifest;protocol=https;branch=android-11-master;manifest=doma.xml;depth=1;directpath=1"
 
-###############################################################################
-# extra layers and files to be put after Yocto's do_unpack into inner builder
-###############################################################################
-# these will be populated into the inner build system on do_unpack_xt_extras
-XT_QUIRK_UNPACK_SRC_URI += " \
-    file://meta-xt-images-extra;subdir=repo \
-    file://xt_reconstruct.bbclass;subdir=repo/meta-xt-images-extra/classes \
+# here we handle prebuild related things
+python __anonymous () {
+    # Depending on XT_ANDROID_PREBUILDS_DIR we either use proprietary sources or use prebuilt graphics
+    if d.getVar("XT_ANDROID_PREBUILDS_DIR", expand=True):
+        DDK_KM_PREBUILT_MODULE = "${XT_ANDROID_PREBUILDS_DIR}/pvr-km/pvrsrvkm.ko"
+        DDK_UM_PREBUILDS = "${XT_ANDROID_PREBUILDS_DIR}/pvr-um/"
+    else:
+        # groups=all results in fetching of proprietary projects
+        d.appendVar("ANDROID_SOURCES", ";groups=all")
+
+    prebuilt_vars = ["DDK_KM_PREBUILT_MODULE", "DDK_UM_PREBUILDS", "TARGET_PREBUILT_KERNEL"]
+    for var in prebuilt_vars:
+        if d.getVar(var, True) != None:
+            if not os.path.exists(d.getVar(var, True)):
+                raise bb.parse.SkipPackage('%s points to a non-existent path' % (var))
+            d.appendVar("PREBUILT_VARS", "%s=%s\n" % (var, d.getVar(var, True)))
+}
+
+SRC_URI = "${ANDROID_SOURCES}"
+
+# SOC_FAMILY is required to be set for proper build of optee, graphic and output folder name.
+# See product's append for properly set value
+SOC_FAMILY ?= "SOC_FAMILY_is_not_set"
+
+# SOC_REVISION is used only for build of graphic drivers.
+# See product's append for possible (but not mandatory) override
+SOC_REVISION = ""
+
+
+# Product and variant are used to form string for 'lunch xenvm-userdebug'
+# These two may be redefined in bbappend-file of local.conf
+ANDROID_PRODUCT ??= "xenvm"
+ANDROID_VARIANT ??= "userdebug"
+
+
+# Disable all not used tasks
+# For android we use only
+# - do_fetch   -- get sources (repo init && sync)
+# - do_compile -- set android environment and build
+# - do_install -- copy assembled images into deploy folder
+# - do_build   -- default start point for build of domain
+do_unpack[noexec] = "1"
+do_patch[noexec] = "1"
+do_configure[noexec] = "1"
+do_populate_sdk[noexec] = "1"
+do_populate_sysroot[noexec] = "1"
+do_collect_build_history[noexec] = "1"
+do_populate_sstate_cache[noexec] = "1"
+
+# This is important workaround.
+# Host python is used to build proper trusted application (TA) for OP-TEE.
+# We can't use android's python because we can't install required library into it.
+# So we have to provide host python with special library.
+# Also pay attention that PYTHON is set by poky, and not need to be set in host
+ANDROID_HOST_PYTHON="$(dirname ${PYTHON})"
+
+EXTRA_OEMAKE_append = " \
+    TARGET_BOARD_PLATFORM=${SOC_FAMILY} \
+    TARGET_SOC_REVISION=${SOC_REVISION} \
+    HOST_PYTHON=${ANDROID_HOST_PYTHON} \
 "
 
-# these layers will be added to bblayers.conf on do_configure
-XT_QUIRK_BB_ADD_LAYER += " \
-    meta-xt-images-extra \
-"
-
-SRC_URI_append = " \
-    file://0001-fetch2-repo-Always-check-if-branch-is-correct.patch;patchdir=poky \
-    file://0002-fetch2-repo-Make-fetcher-always-sync-on-unpack.patch;patchdir=poky \
-    file://0003-fetch2-repo-Use-multiple-jobs-to-fetch-and-sync.patch;patchdir=poky \
-    file://0004-Make-pack-unpack-multithreaded.patch;patchdir=poky \
-    file://0005-fetch2-repo-Add-group-parameter.patch;patchdir=poky \
-"
-
-update_local_conf() {
-    local local_conf="${S}/build/conf/local.conf"
-
+# Let android build system clarify what need to be build
+do_compile[nostamp] = "1"
+do_compile() {
     cd ${S}
+    USRBINPATH_NATIVE="${RECIPE_SYSROOT_NATIVE}/usr/bin"
 
-    # override what xt-distro wants as machine as we will only use
-    # machines understood by Poky
-    base_update_conf_value ${local_conf} MACHINE "qemux86-64"
-
-    if [ ! -z ${XT_ANDROID_PREBUILDS_DIR} ];then
-        base_set_conf_value ${local_conf} DDK_KM_PREBUILT_MODULE "${XT_ANDROID_PREBUILDS_DIR}/pvr-km/pvrsrvkm.ko"
-        base_set_conf_value ${local_conf} DDK_UM_PREBUILDS "${XT_ANDROID_PREBUILDS_DIR}/pvr-um/"
-    fi
+    # run Android build in sane environment
+    # Pay attention: EXTRA_OEMAKE includes multithread option '-j' (see poky sources)
+    env -i HOME="$HOME" USER="$USER" \
+           PATH="${USRBINPATH_NATIVE}:${PATH}" \
+           ${PREBUILT_VARS} \
+           bash -c "source build/envsetup.sh && \
+                    lunch ${ANDROID_PRODUCT}-${ANDROID_VARIANT} && \
+                    make ${EXTRA_OEMAKE} \
+           "
 }
 
-python do_configure_append() {
-    bb.build.exec_func("update_local_conf", d)
-}
 
-# we do not have an SDK to populate
-do_populate_sdk() {
-}
+# Always install latest android images
+do_install[nostamp] = "1"
+do_install() {
+    install -d "${DEPLOY_DIR}/${BPN}/images"
+    install -d "${XT_DIR_ABS_SHARED_BOOT_DOMA}"
 
-# populate Android ccache as well
-do_populate_sstate_cache_append() {
-    if [ -n "${EXPANDED_XT_POPULATE_SSTATE_CACHE}" ] ; then
-        CURRENT_CCACHE=$(ls ${SSTATE_DIR}/ | grep "ccache")
-        if [ -n "${CURRENT_CCACHE}" ] && [ -d ${SSTATE_DIR}/${CURRENT_CCACHE} ]; then
-            if [ -d ${XT_SSTATE_CACHE_MIRROR_DIR}/${CURRENT_CCACHE} ]; then
-                rm -rf  ${XT_SSTATE_CACHE_MIRROR_DIR}/${CURRENT_CCACHE}
-            fi
-            mv -f ${SSTATE_DIR}/${CURRENT_CCACHE} ${XT_SSTATE_CACHE_MIRROR_DIR}/ || true
-        fi
-    fi
+    # copy images to the deploy directory
+    find "${S}/out/target/product/${ANDROID_PRODUCT}" -maxdepth 1 -iname '*.img' -exec \
+        cp -f --no-dereference --preserve=links {} "${DEPLOY_DIR}/${BPN}/images" \;
+
+    # TODO assemble android's images into single image
 }

recipes-domu/domu-image-android/files/0001-fetch2-repo-Always-check-if-branch-is-correct.patch
@@ -1,35 +1,0 @@
-From a733c75f3a0918d85e433e4fc567cb3b9949858b Mon Sep 17 00:00:00 2001
-From: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
-Date: Tue, 24 Oct 2017 10:04:03 +0300
-Subject: [PATCH 1/4] fetch2/repo: Always check if branch is correct
-
-Current implementation doesn't pay attention if
-the same repository is used for manifest, but different
-branch, e.g. existing cached repository is not checked
-if the branch is correct. Fix this by always running
-repo init/sync.
-
-Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
----
- bitbake/lib/bb/fetch2/repo.py | 5 ++---
- 1 file changed, 2 insertions(+), 3 deletions(-)
-
-diff --git a/bitbake/lib/bb/fetch2/repo.py b/bitbake/lib/bb/fetch2/repo.py
-index 1be91cc698e3..6b2efa2b91f8 100644
---- a/bitbake/lib/bb/fetch2/repo.py
-+++ b/bitbake/lib/bb/fetch2/repo.py
-@@ -70,9 +70,8 @@ class Repo(FetchMethod):
- 
-         repodir = os.path.join(codir, "repo")
-         bb.utils.mkdirhier(repodir)
--        if not os.path.exists(os.path.join(repodir, ".repo")):
--            bb.fetch2.check_network_access(d, "repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), ud.url)
--            runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=repodir)
-+        bb.fetch2.check_network_access(d, "repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), ud.url)
-+        runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=repodir)
- 
-         bb.fetch2.check_network_access(d, "repo sync %s" % ud.url, ud.url)
-         runfetchcmd("repo sync", d, workdir=repodir)
--- 
-2.7.4
-

recipes-domu/domu-image-android/files/0002-fetch2-repo-Make-fetcher-always-sync-on-unpack.patch
@@ -1,74 +1,0 @@
-From d8cc9c16df233abfc39efbc04e37a795134588bd Mon Sep 17 00:00:00 2001
-From: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
-Date: Thu, 26 Oct 2017 17:16:49 +0300
-Subject: [PATCH 2/4] fetch2/repo: Make fetcher always sync on unpack
-
-Currently if repo has fetched the repository and
-cached it to a tar file it never runs repo sync again,
-thus making the build use potentially outdated repo.
-Fix this by implementing unpack functionality which runs
-repo sync.
-
-Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
----
- bitbake/lib/bb/fetch2/repo.py | 22 +++++++++++++---------
- 1 file changed, 13 insertions(+), 9 deletions(-)
-
-diff --git a/bitbake/lib/bb/fetch2/repo.py b/bitbake/lib/bb/fetch2/repo.py
-index 6b2efa2b91f8..d51d1ebf7a07 100644
---- a/bitbake/lib/bb/fetch2/repo.py
-+++ b/bitbake/lib/bb/fetch2/repo.py
-@@ -51,6 +51,10 @@ class Repo(FetchMethod):
-             ud.manifest += '.xml'
- 
-         ud.localfile = d.expand("repo_%s%s_%s_%s.tar.gz" % (ud.host, ud.path.replace("/", "."), ud.manifest, ud.branch))
-+        repodir = d.getVar("REPODIR") or os.path.join(d.getVar("DL_DIR"), "repo")
-+        gitsrcname = "%s%s" % (ud.host, ud.path.replace("/", "."))
-+        ud.codir = os.path.join(repodir, gitsrcname, ud.manifest)
-+        ud.repodir = os.path.join(ud.codir, "repo")
- 
-     def download(self, ud, d):
-         """Fetch url"""
-@@ -59,22 +63,17 @@ class Repo(FetchMethod):
-             logger.debug(1, "%s already exists (or was stashed). Skipping repo init / sync.", ud.localpath)
-             return
- 
--        gitsrcname = "%s%s" % (ud.host, ud.path.replace("/", "."))
--        repodir = d.getVar("REPODIR") or os.path.join(d.getVar("DL_DIR"), "repo")
--        codir = os.path.join(repodir, gitsrcname, ud.manifest)
--
-         if ud.user:
-             username = ud.user + "@"
-         else:
-             username = ""
- 
--        repodir = os.path.join(codir, "repo")
--        bb.utils.mkdirhier(repodir)
-+        bb.utils.mkdirhier(ud.repodir)
-         bb.fetch2.check_network_access(d, "repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), ud.url)
--        runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=repodir)
-+        runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=ud.repodir)
- 
-         bb.fetch2.check_network_access(d, "repo sync %s" % ud.url, ud.url)
--        runfetchcmd("repo sync", d, workdir=repodir)
-+        runfetchcmd("repo sync", d, workdir=ud.repodir)
- 
-         scmdata = ud.parm.get("scmdata", "")
-         if scmdata == "keep":
-@@ -83,7 +82,12 @@ class Repo(FetchMethod):
-             tar_flags = "--exclude='.repo' --exclude='.git'"
- 
-         # Create a cache
--        runfetchcmd("tar %s -czf %s %s" % (tar_flags, ud.localpath, os.path.join(".", "*") ), d, workdir=codir)
-+        runfetchcmd("tar %s -czf %s %s" % (tar_flags, ud.localpath, os.path.join(".", "*") ), d, workdir=ud.codir)
-+
-+    def unpack(self, ud, destdir, d):
-+        FetchMethod.unpack(self, ud, destdir, d)
-+        bb.fetch2.check_network_access(d, "repo sync %s" % ud.url, ud.url)
-+        runfetchcmd("repo sync", d, workdir=destdir)
- 
-     def supports_srcrev(self):
-         return False
--- 
-2.7.4
-

recipes-domu/domu-image-android/files/0003-fetch2-repo-Use-multiple-jobs-to-fetch-and-sync.patch
@@ -1,56 +1,0 @@
-From 199868288780e6eaa0a7632d2de80e2c84f9d73d Mon Sep 17 00:00:00 2001
-From: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
-Date: Tue, 24 Oct 2017 10:14:14 +0300
-Subject: [PATCH 3/4] fetch2/repo: Use multiple jobs to fetch and sync
-
-Google repo supports "--jobs" parameter, so one can
-speed up the process of fetching and syncing. According to
-https://groups.google.com/a/chromium.org/forum/#!topic/chromium-os-dev/ny3nkgE9AE8
-it is even more gets to speed if used separately for
-network jobs and "local" ones.
-Use BB_NUMBER_THREADS to run parallel repo sync.
-
-Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
----
- bitbake/lib/bb/fetch2/repo.py | 11 +++++++++--
- 1 file changed, 9 insertions(+), 2 deletions(-)
-
-diff --git a/bitbake/lib/bb/fetch2/repo.py b/bitbake/lib/bb/fetch2/repo.py
-index d51d1ebf7a07..0ed0f923f6a4 100644
---- a/bitbake/lib/bb/fetch2/repo.py
-+++ b/bitbake/lib/bb/fetch2/repo.py
-@@ -56,6 +56,13 @@ class Repo(FetchMethod):
-         ud.codir = os.path.join(repodir, gitsrcname, ud.manifest)
-         ud.repodir = os.path.join(ud.codir, "repo")
- 
-+    def sync(self, destdir, d):
-+        # repo can separate the "network" sync (the git fetch part, network bound)
-+        # from the "local" sync (the git rebase part, compute & i/o bound).
-+        num_jobs = d.getVar("BB_NUMBER_THREADS", True) or "1"
-+        runfetchcmd("repo sync -c -n -j%s" % num_jobs, d, workdir=destdir)
-+        runfetchcmd("repo sync -c -l -j%s" % num_jobs, d, workdir=destdir)
-+
-     def download(self, ud, d):
-         """Fetch url"""
- 
-@@ -73,7 +80,7 @@ class Repo(FetchMethod):
-         runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=ud.repodir)
- 
-         bb.fetch2.check_network_access(d, "repo sync %s" % ud.url, ud.url)
--        runfetchcmd("repo sync", d, workdir=ud.repodir)
-+        self.sync(ud.repodir, d)
- 
-         scmdata = ud.parm.get("scmdata", "")
-         if scmdata == "keep":
-@@ -87,7 +94,7 @@ class Repo(FetchMethod):
-     def unpack(self, ud, destdir, d):
-         FetchMethod.unpack(self, ud, destdir, d)
-         bb.fetch2.check_network_access(d, "repo sync %s" % ud.url, ud.url)
--        runfetchcmd("repo sync", d, workdir=destdir)
-+        self.sync(os.path.join(destdir, "repo"), d)
- 
-     def supports_srcrev(self):
-         return False
--- 
-2.7.4
-

recipes-domu/domu-image-android/files/0004-Make-pack-unpack-multithreaded.patch
@@ -1,44 +1,0 @@
-From 6526322512920e9b020fffe948100d024ee19774 Mon Sep 17 00:00:00 2001
-From: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
-Date: Fri, 29 Dec 2017 17:26:49 +0200
-Subject: [PATCH 4/4] Make pack/unpack multithreaded
-
-Use pigz/unpigz to make fetcher's pack/unpack tar
-run in parallel.
-
-Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
-Signed-off-by: Artem Mygaiev <artem_mygaiev@epam.com>
----
- bitbake/lib/bb/fetch2/__init__.py | 2 +-
- bitbake/lib/bb/fetch2/repo.py     | 2 +-
- 2 files changed, 2 insertions(+), 2 deletions(-)
-
-diff --git a/bitbake/lib/bb/fetch2/__init__.py b/bitbake/lib/bb/fetch2/__init__.py
-index b853da30bdbf..2a0f875dd394 100644
---- a/bitbake/lib/bb/fetch2/__init__.py
-+++ b/bitbake/lib/bb/fetch2/__init__.py
-@@ -1398,7 +1398,7 @@ class FetchMethod(object):
- 
-         if unpack:
-             if file.endswith('.tar'):
--                cmd = 'tar x --no-same-owner -f %s' % file
-+                cmd = 'unpigz < %s | tar x --no-same-owner' % file
-             elif file.endswith('.tgz') or file.endswith('.tar.gz') or file.endswith('.tar.Z'):
-                 cmd = 'tar xz --no-same-owner -f %s' % file
-             elif file.endswith('.tbz') or file.endswith('.tbz2') or file.endswith('.tar.bz2'):
-diff --git a/bitbake/lib/bb/fetch2/repo.py b/bitbake/lib/bb/fetch2/repo.py
-index 0ed0f923f6a4..adf437a24e32 100644
---- a/bitbake/lib/bb/fetch2/repo.py
-+++ b/bitbake/lib/bb/fetch2/repo.py
-@@ -89,7 +89,7 @@ class Repo(FetchMethod):
-             tar_flags = "--exclude='.repo' --exclude='.git'"
- 
-         # Create a cache
--        runfetchcmd("tar %s -czf %s %s" % (tar_flags, ud.localpath, os.path.join(".", "*") ), d, workdir=ud.codir)
-+        runfetchcmd("tar %s -cf - %s | pigz > %s" % (tar_flags, os.path.join(".", "*"), ud.localpath), d, workdir=ud.codir)
- 
-     def unpack(self, ud, destdir, d):
-         FetchMethod.unpack(self, ud, destdir, d)
--- 
-2.7.4
-

recipes-domu/domu-image-android/files/0005-fetch2-repo-Add-group-parameter.patch
@@ -1,28 +1,0 @@
-From f9b84d2ae9af8e4aa23f3d29b2c5675b80a38c1f Mon Sep 17 00:00:00 2001
-From: xen-troops-buildbot <not.real.email@epam.com>
-Date: Tue, 11 Dec 2018 15:10:42 +0000
-Subject: [PATCH] Add group parameter
-
----
- bitbake/lib/bb/fetch2/repo.py | 6 +++++-
- 1 file changed, 5 insertions(+), 1 deletion(-)
-
-diff --git a/bitbake/lib/bb/fetch2/repo.py b/bitbake/lib/bb/fetch2/repo.py
-index ff4d548..ba6df27 100644
---- a/bitbake/lib/bb/fetch2/repo.py
-+++ b/bitbake/lib/bb/fetch2/repo.py
-@@ -77,7 +77,11 @@ class Repo(FetchMethod):
-
-         bb.utils.mkdirhier(ud.repodir)
-         bb.fetch2.check_network_access(d, "repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), ud.url)
--        runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=ud.repodir)
-+        prebuilds = d.getVar("DDK_UM_PREBUILDS") or ""
-+        if prebuilds:
-+            runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path), d, workdir=ud.repodir)
-+        else:
-+            runfetchcmd("repo init -m %s -b %s -u %s://%s%s%s -g %s" % (ud.manifest, ud.branch, ud.proto, username, ud.host, ud.path, "all"), d, workdir=ud.repodir)
-
-         bb.fetch2.check_network_access(d, "repo sync %s" % ud.url, ud.url)
-         self.sync(ud.repodir, d)
---
-2.7.4

recipes-domu/domu-image-android/files/meta-xt-images-extra/conf/layer.conf
@@ -1,12 +1,0 @@
-# We have a conf and classes directory, add to BBPATH
-BBPATH =. "${LAYERDIR}:"
-
-# We have recipes-* directories, add to BBFILES
-BBFILES += "${LAYERDIR}/recipes-*/*/*.bb \
-            ${LAYERDIR}/recipes-*/*/*.bbappend"
-
-BBFILE_COLLECTIONS += "meta-xt-images-extra"
-BBFILE_PATTERN_meta-xt-images-extra := "^${LAYERDIR}/"
-BBFILE_PRIORITY_meta-xt-images-extra = "6"
-
-LAYERSERIES_COMPAT_meta-xt-images-extra = "sumo thud zeus dunfell"

recipes-domu/domu-image-android/files/meta-xt-images-extra/recipes-extended/android/android.bb
@@ -1,53 +1,0 @@
-SUMMARY = "Android"
-DESCRIPTION = "Recipe for building Android image"
-
-LICENSE = "Apache-2.0"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
-
-S = "${WORKDIR}/repo"
-
-SRCREV = "${AUTOREV}"
-
-require inc/xt_shared_env.inc
-
-inherit pythonnative
-inherit xt_reconstruct
-
-# remove all depends normally required for oe_make, e.g. cross-compiler
-# and tools: these are not needed for Android
-BASEDEPENDS = ""
-
-DEPENDS += "lz4-native bc-native python-pycrypto-native curl-native rsync-native bison-native coreutils-native unzip-native zip-native"
-
-ANDROID_PRODUCT ?= "aosp_arm64"
-ANDROID_VARIANT ?= "eng"
-SOC_FAMILY ?= "default"
-
-ANDROID_OUT_DIR_COMMON_BASE = ""
-
-python __anonymous () {
-    prebuilt_vars = ["DDK_KM_PREBUILT_MODULE", "TARGET_PREBUILT_KERNEL", "DDK_UM_PREBUILDS"]
-    for var in prebuilt_vars:
-        if d.getVar(var, True) != None:
-            if not os.path.exists(d.getVar(var, True)):
-                raise bb.parse.SkipPackage('%s points to a non-existent path' % (var))
-            d.appendVar("DDK_PREBUILT_VARS", "%s=%s\n" % (var, d.getVar(var, True)))
-}
-
-do_configure() {
-}
-
-do_compile() {
-    cd ${S}
-    USRBINPATH_NATIVE="${RECIPE_SYSROOT_NATIVE}/usr/bin"
-
-    # run Android build in sane environment
-    env -i HOME="$HOME" LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" USER="$USER" \
-           PATH="${USRBINPATH_NATIVE}:${PATH}" \
-           OUT_DIR_COMMON_BASE="${ANDROID_OUT_DIR_COMMON_BASE}" \
-           ${DDK_PREBUILT_VARS} \
-           bash -c "source build/envsetup.sh && \
-                    lunch ${ANDROID_PRODUCT}-${ANDROID_VARIANT} && \
-                    make ${EXTRA_OEMAKE} ${PARALLEL_MAKE} \
-           "
-}

commit 3710cf1a3900e37cb91a3769bff4805390952ae6

doma: Fix include of xt_shared_env.inc
Fixes: 10628d1 "doma: rework Android build"

Signed-off-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>
Reviewed-by: Valerii Chubar <valerii_chubar@epam.com>

recipes-domu/domu-image-android/domu-image-android.bb
@@ -2,7 +2,7 @@ SUMMARY = "Base for Android build"
 
 LICENSE = "MIT"
 
-require inc/xt_shared_env.inc
+require recipes-domx/meta-xt-prod-domx/inc/xt_shared_env.inc
 
 # Important note.
 # We use REPODIR and SRC_URI with 'directpath=1' to bypass fetcher's